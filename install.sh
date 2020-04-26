c1e8521ccdeeeee2b824470cc504f6569acdfb4d
Mihai Cristescu <mihai.cristescu@gmail.com>
2020-04-26 17:10:19 +0300

    Updates

--- install.sh
+++ install.sh
@@ -5,6 +5,8 @@ SOURCE_PATH="$(cd "$SCRIPT" || exit 1 ; pwd -P )/Source"
 pushd "$SOURCE_PATH" >/dev/null || printf "Error. Exiting.." >&2
 FILES=$(find . -type f -print | sed "s|^\./||" )
 for FILE in $FILES; do
+    echo "Linking $FILE"
     ln -f -s "$SOURCE_PATH/$FILE" ~/"$FILE"
 done
-popd || exit 1
+popd >/dev/null || exit 1A
+echo "Done."
