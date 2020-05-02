a18b4ad223b868b20f962be1a936d05d9d623b92
Mihai Cristescu <mihai.cristescu@gmail.com>
2020-05-02 13:50:45 +0300

    Updates

--- install.sh
+++ install.sh
@@ -1,12 +1,14 @@
-#! /usr/bin/env bash
-
+#! /usr/bin/env zsh
+set -x
 SCRIPT=$(dirname "$0")
 SOURCE_PATH="$(cd "$SCRIPT" || exit 1 ; pwd -P )/Source"
 pushd "$SOURCE_PATH" >/dev/null || printf "Error. Exiting.." >&2
 FILES=$(find . -type f -print | sed "s|^\./||" )
+FILES=("${(f)FILES}")
 for FILE in $FILES; do
     echo "Linking $FILE"
     ln -f -s "$SOURCE_PATH/$FILE" ~/"$FILE"
 done
 popd >/dev/null || exit 1
 echo "Done."
+set +x
