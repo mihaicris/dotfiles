5c18efb3262f70ab2059365354757973d0c4a098
Mihai Cristescu <mihai.cristescu@adoreme.com>
2023-10-01 08:29:10 +0300

    Updates

--- scripts/freezeInterfaceBuilderFiles.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env zsh
-
-find . \
-    -path "./Pods/*" \
-    -prune -o \
-    -type f \
-    \( -name "*.xib" -or -name "*.storyboard" \) \
-    -exec sed -i '' 's/propertyAccessControl=\"none\"/propertyAccessControl=\"all\"/' {} \;
-
